7d2cb784362bca85ceac568d5e32cc8047f7e810
Willian C. Nunes <69167321+troopsofarchangel@users.noreply.github.com>
2025-06-11 08:53:22 -0300

    Add files via upload

---
+++ hooks/useLocalStorage.ts
@@ -0,0 +1,28 @@
+
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage<T,>(key: string, initialValue: T): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const getStoredValue = (): T => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.warn(`Error reading localStorage key "${key}":`, error);
+      return initialValue;
+    }
+  };
+
+  const [storedValue, setStoredValue] = useState<T>(getStoredValue);
+
+  useEffect(() => {
+    try {
+      const valueToStore = storedValue instanceof Function ? storedValue(getStoredValue()) : storedValue;
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.warn(`Error setting localStorage key "${key}":`, error);
+    }
+     // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [key, storedValue]);
+
+  return [storedValue, setStoredValue];
+}
